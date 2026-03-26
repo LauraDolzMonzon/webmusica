aba8aaa4b0d995b587e89333a5e5dff42ffb8a1c
LauraDolzMonzon <lauradolzm@gmail.com>
2026-03-26 09:16:43 +0100

    Add files via upload

---
+++ validaciones_login_programacion_y_noticias.js
@@ -0,0 +1,23 @@
+              document.addEventListener("DOMContentLoaded", function () { /*Sirve para ejecutar JavaScript cuando el DOM de la página ya está cargado.*/
+                /*noticias y progamacion*/
+
+             
+                    /*login formulario invetario*/
+
+                const usuarioInput = document.getElementById("usuariofyp");
+                usuarioInput.oninvalid = function () {
+                    usuarioInput.setCustomValidity("Se requiere 8 números y una letra mayúscula");
+                };
+                usuarioInput.oninput = function () {
+                    usuarioInput.setCustomValidity("");
+                };
+
+                const contrasenaInput = document.getElementById("contrasenafyp");
+                contrasenaInput.oninvalid = function () {
+                    contrasenaInput.setCustomValidity("Se requiere como mínimo 8 caracteres");
+                };
+                contrasenaInput.oninput = function () {
+                    contrasenaInput.setCustomValidity("");
+                };
+
+});
