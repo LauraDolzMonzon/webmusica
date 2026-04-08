a5f92fea0581127cca6032b8fcb2c9e1a05e01a4
LauraDolzMonzon <lauradolzm@gmail.com>
2026-04-08 10:08:35 +0200

    Add files via upload

---
+++ php/controladorloginfyn.php
@@ -0,0 +1,29 @@
+<?php
+  session_start();
+  $servidor = "127.0.0.1";
+  $basededatos = "webmusica";
+  $useario = "root";
+  $contrasenna = "";
+  $conn = new mysqli($servidor, $useario, $contrasenna, $basededatos);
+  if ($conn->connect_error){
+    die ("error de conexion" . $conn->connect_error);
+  }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuariofyp = trim($_POST['usuariofyp']);
+    $contrasenafyp = trim($_POST['contrasenafyp']);
+    $sql2 = "SELECT dni, contrasenya, rol FROM profesor WHERE dni = '$usuariofyp'";
+    $resultado2 = mysqli_query($conn, $sql2);
+    if ($resultado2 && mysqli_num_rows($resultado2) === 1) {
+        $fila2 = mysqli_fetch_assoc($resultado2);
+       if ($fila2['contrasenya'] === $contrasenafyp && $fila2['rol'] === 'admin') {
+            session_regenerate_id(true);
+            $_SESSION['dni'] = $fila2['dni'];
+            $_SESSION['rol'] = $fila2['rol'];
+            header("Location: formulario_programacion_y_noticias.php");
+            exit();
+        }
+    } 
+    header("Location: login_formulario_noticias_y_programacion.php");
+    exit();  
+  }
+?>
---
+++ php/noticias_y_programacion.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link  rel="stylesheet" href="../estilos.css">
+            <title> Noticias y programaci&oacute;n</title>
+    
+
+        </head>
+
+
+        <body>
+            <nav>       
+            <ul>   
+                <a href="index.php">Inicio</a>
+                <a href="#">Noticias y programaci&oacute;n</a>  
+                <a href="login_inventario.php">Inventario</a>
+                <a href="login_formulario_inventario.php">Formulario de inventario</a>
+                <a href="login_formulario_noticias_y_programacion.php">Formulario de noticias y formulario de programaci&oacute;n</a>
+                <a href="formulario_contacto.php">Contacto</a>
+            </ul>
+        </nav>  
+        <header>
+            <h2>Noticias y Programaci&oacute;n</h2>
+
+        </header>
+            
+        <main>
+            <h3 class="h3nyp">Noticias</h3>
+            <div> 
+              <table>
+                <tr>
+                   <th>Fecha</th>
+                    <th>Noticias</th>
+                    <th>Lugar</th>
+                    <th>Texto</th>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>  
+                 <tr>
+                   <th>Fecha</th>
+                    <th>Noticias</th>
+                    <th>Lugar</th>
+                    <th>Texto</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>  
+              </table>
+            </div>  
+            <h3 class="h3nyp">Programaci&oacute;n</h3>  
+            <div>
+                <table>
+                  <tr>
+                    <th>Curso</th>
+                    <th>A&ntilde;o</th>
+                    <th>Profesor</th>
+                    <th>Programaci&oacute;n</th>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    
+                    <td><button>Descarga</button> </td>
+
+                </tr>  
+                 <tr>
+                   <th>Curso</th>
+                   <th>A&ntilde;o</th>
+                   <th>Profesor</th>
+                   <th>Programaci&oacute;n</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button>Descarga</button> </td>
+                </tr>   
+                </table>
+           
+           </div>     
+        </main>
+        
+   
+        </main>
+        <footer>
+            <h4>Bienvenido a la web del Departamento de M&uacute;sica del IES Bajo Arag&oacute;n.</h4>
+
+        </footer>
+
+        </body>
+
+    </html>    
