8ca33efbbcac3b94707b395a6292c078658107cd
LauraDolzMonzon <lauradolzm@gmail.com>
2026-04-09 14:36:12 +0200

    Add files via upload

---
+++ php/formulario_inventario.php
@@ -0,0 +1,122 @@
+<?php
+    session_start();
+    require __DIR__. "/conexion.php";
+    if (!isset($_SESSION['rol'])){
+        header("Location: login_formulario_inventario.php");
+        exit;
+    }
+?>
+<!DOCTYPE html>
+  <html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="../validaciones_inventario.js"> </script>
+        <link  rel="stylesheet" href="../estilos.css">
+        <title> Inventario</title>
+
+
+
+    </head>
+    <body>
+        <nav>       
+            <ul>   
+                <a href="index.php">Inicio</a>
+                <a href="noticias_y_programacion.php">Noticias y programaci&oacute;n</a>
+                <a href="login_inventario.php">Inventario</a>
+                <a href="login_formulario_inventario.php">Formulario de inventario</a>
+                <a href="login_formulario_noticias_y_programacion.php">Formulario de noticias y formulario de programaci&oacute;n</a>
+                <a href="formulario_contacto.php">Contacto</a>
+            </ul>
+        </nav>
+        <header><h2>Inventario</h2></header>        
+        <main>
+            <h3>A&ntilde;adir instrumento</h3>
+            <form action="controladorformularioinventario.php" method="post">
+              <table>
+                <tr>
+                    <th><label for="Intrumetroivenatario">Instrumento</label></th>
+                    <th><label for="invetariofamilia">Familia</label></th>
+                    <th><label for="ivetarioubicacion">Aula</label> </th>
+                    
+                    <th><label for="invetariounidades">Unidades</label></th>
+                    <th><label for="invetarioanodeadquision">A&ntilde;o de adquisici&oacute;n</label></th>
+                    <th><label for="botonadminstrainventario">Enviar</label></th>
+                </tr>
+                <tr>
+                    <td><input type="text" id="Intrumetroivenatario" name="Intrumetroivenatario" pattern="[A-Z0a-zñÑ]+" required> </td>
+               
+                    <td>
+                    
+                        <select name="invetariofamilia" id="invetariofamilia" required>
+                            <option value="sinespecificarfamilia2">Sinespecificarfamilia</soption>
+                            <option value="vientometral">Viento metal</option>
+                            <option value="vientomadera">Viento madera</option>
+                            <option value="pesucion">Percusi&oacute;n</option>
+                            <option value="cuerda">Cuerda</option>
+                        </select> 
+                    </td>
+                    <td>  <select name="ivetarioubicacion" id="ivetarioubicacion" required>
+                            <option value="sinespecificarubicacion2">Sinespecificarubicacion</option>
+                            <option value="RMU1">RMU1</option>
+                            <option value="RMU2">RMU2</option>
+                        </select>
+                    </td>
+                    <td><input type="text" id="invetariounidades" name="invetariounidades" pattern="^[0-9]+$" required> </td> 
+                    
+
+                    
+                       
+
+                    <td><input type="date" id="invetarioanodeadquision" name="invetarioanodeadquision" pattern="^[0-9]{4}$" maxlength="4" minlength="4" required> </td>
+                   
+                    <td><button type="submit" class="botonadminstrainventario">Enviar</button></td>
+                 </tr>  
+               </table>
+               <h3>Administrar instrumentos</h3>
+            </form>        
+            <div > 
+               <form action="controlador.php"     method="post">
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Instrumento</th>
+                        <th>Familia</th>
+                        <th>Ubicaci&oacute;n</th>
+                        <th>Unidades</th>
+                        <th>A&ntilde;o de adquisici&oacute;n</th>
+                        <th Colspan="2">Administrar</th>
+                        
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><button class="botonadminstrainventarioeditaryeliminar" >Editar</button></td>
+                        <td><button class="botonadminstrainventarioeditaryeliminar">Eliminar</button></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><button class="botonadminstrainventarioeditaryeliminar">Editar</button></td>
+                        <td><button class="botonadminstrainventarioeditaryeliminar">Eliminar</button></td>
+                    </tr>
+                </table>
+              </form>  
+           </div>     
+        </main>
+        <footer>    
+            <h4>Bienvenido a la web del Departamento de M&uacute;sica del IES Bajo Arag&oacute;n.</h4>
+
+        </footer>
+        
+    </body>
+  </html>
---
+++ php/noticias_y_programacion.php
@@ -0,0 +1,126 @@
+<?php
+     session_start();
+    $servidor = "127.0.0.1";
+    $basededatos = "webmusica";
+    $useario = "root";
+    $contrasenna = "";
+    $conn = new mysqli($servidor, $useario, $contrasenna, $basededatos);
+    if ($conn->connect_error){
+        die("error de conexion" . $conn->connect_error);
+    }
+    $sql4 = "SELECT  titulo_noticia, fecha, lugar,  texto_noticia, dni_profesor_noticia FROM noticia ORDER BY fecha DESC";
+    $resultadostablanoticias = $conn->query($sql4)
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link  rel="stylesheet" href="../estilos.css">
+            <title> Noticias y programaci&oacute;n</title>
+    
+
+        </head>
+
+
+        <body>
+            <nav>       
+            <ul>   
+                <a href="index.php">Inicio</a>
+                <a href="#">Noticias y programaci&oacute;n</a>  
+                <a href="login_inventario.php">Inventario</a>
+                <a href="login_formulario_inventario.php">Formulario de inventario</a>
+                <a href="login_formulario_noticias_y_programacion.php">Formulario de noticias y formulario de programaci&oacute;n</a>
+                <a href="formulario_contacto.php">Contacto</a>
+            </ul>
+        </nav>  
+        <header>
+            <h2>Noticias y Programaci&oacute;n</h2>
+
+        </header>
+            
+        <main>
+            <h3 class="h3nyp">Noticias</h3>
+            <div> 
+              <?php
+                if ($resultadostablanoticias->num_rows > 0){
+                    while($row = $resultadostablanoticias->fetch_assoc()) {   
+                        echo  "<table class=\"tablanoticias\">";
+                        echo     "<tr>";
+                        echo        "<th>Fecha</th>";
+                        echo        "<th>Noticias</th>";
+                        echo        "<th>Lugar</th>";
+                        echo         "<th>Texto</th>";
+                        echo     "</tr>";
+                        echo     "<tr>";
+                        echo        "<td>" . htmlspecialchars($row['fecha']) . "</td>";
+                        echo        "<td>" . htmlspecialchars($row['titulo_noticia']) . "</td>";
+                        echo        "<td>" . htmlspecialchars($row['lugar']) . "</td>";
+                        echo        "<td>" . htmlspecialchars($row['texto_noticia']) . "</td>";
+                        echo     "</tr>";   
+                        echo  "</table>";
+                    }
+                 } else {
+                      echo  "<table>";
+                        echo     "<tr>";
+                        echo        "<th>Fecha</th>";
+                        echo        "<th>Noticias</th>";
+                        echo        "<th>Lugar</th>";
+                        echo         "<th>Texto</th>";
+                        echo     "</tr>";
+                         
+                        echo     "<tr >";
+                        echo        "<td colspan='4'>No hay datos</td>";
+                        
+                        echo     "</tr>";
+                 }
+             ?>
+
+
+            </div>  
+            <h3 class="h3nyp">Programaci&oacute;n</h3>  
+            <div>
+                <table>
+                  <tr>
+                    <th>Curso</th>
+                    <th>A&ntilde;o</th>
+                    <th>Profesor</th>
+                    <th>Programaci&oacute;n</th>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    
+                    <td><button>Descarga</button> </td>
+
+                </tr>  
+                 <tr>
+                   <th>Curso</th>
+                   <th>A&ntilde;o</th>
+                   <th>Profesor</th>
+                   <th>Programaci&oacute;n</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button>Descarga</button> </td>
+                </tr>   
+                </table>
+           
+           </div>     
+        </main>
+        
+   
+        </main>
+        <footer>
+            <h4>Bienvenido a la web del Departamento de M&uacute;sica del IES Bajo Arag&oacute;n.</h4>
+
+        </footer>
+
+        </body>
+
+    </html>    
